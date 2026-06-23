d552f5547e475861dc2437b6974576fb29dc4a37
siddhants0611 <siddhants0611@gmail.com>
2026-06-23 20:33:27 +0530

    intial push
    To Do List Project

---
+++ script.js
@@ -0,0 +1,35 @@
+function addTask() {
+
+    let input = document.getElementById("taskInput");
+
+    let taskText = input.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+        <span onclick="toggleTask(this)">
+            ${taskText}
+        </span>
+
+        <button class="deleteBtn" onclick="deleteTask(this)">
+            Delete
+        </button>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
+
+function deleteTask(button) {
+    button.parentElement.remove();
+}
+
+function toggleTask(task) {
+    task.classList.toggle("completed");
+}
